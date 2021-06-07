3f531e49b43d74822730f517f1eadc90fc599cb6
Ramesh Baskarasubramanian <ramesh.baskarasubramanian@live.com>
2021-06-07 08:05:54 +0800

    Directly using cy commands in page objects and tests for more readability

--- cypress/integration/home_page.spec.js
+++ cypress/integration/home_page.spec.js
@@ -1,19 +1,18 @@
 import * as HomePage from '../lib/pages/homePage';
-import * as Page from '../lib/pages/page';
 
 describe('user', () => {
   before(function () {
-    Page.visit('https://www.google.com');
+    cy.visit('https://www.google.com');
   });
 
   // This test intentionaly fails to demonstrate the reporting
   it('should be able to visit the home page', function () {
     cy.log('This test intentionaly fails to demonstrate the reporting and attaching of screenshots!');
-    Page.urlShouldHavePath('gogle.com');
+    cy.url().should('contain', 'gogle.com');
   });
 
   it('should be able to search for a term', function () {
     HomePage.search('Cypress');
-    Page.pageShouldHaveText('www.cypress.io');
+    cy.contains('www.cypress.io').should('not.be.undefined');
   });
 });
--- cypress/lib/pages/homePage.js
+++ cypress/lib/pages/homePage.js
@@ -1,10 +1,8 @@
-import * as Page from './page';
-
 // Elements
 const tbSearch = `input[name='q']`;
 
 // Functions
 export function search(text) {
   cy.log(`HomePage: Search for text: ${text}`);
-  Page.typeAndSubmit(tbSearch, text);
+  cy.get(tbSearch).clear().type(`${text}{enter}`);
 }
